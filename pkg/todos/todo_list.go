56253855928802569b722863a230d602553267b4
nchern <nchern@inbox.ru>
2020-03-09 21:34:15 +0100

    hierarchical todos: working impl

--- pkg/todos/todo_list.go
+++ pkg/todos/todo_list.go
@@ -7,12 +7,109 @@ import (
 	"regexp"
 )
 
-var (
-	todoItemUndoneRx = regexp.MustCompile(`(?i)^\s*?-\s*?\[\s?\]\s+?.+$`)
-	todoItemDoneRx   = regexp.MustCompile(`(?i)^\s*?-\s*?\[x\]\s+?.+$`)
-
-	whitespaceRx = regexp.MustCompile(`^\s*$`)
+const (
+	other kind = iota
+	done
+	blank
+	notDone
 )
+
+var (
+	whitespaceRx = regexp.MustCompile(`^\s*$`)
+
+	todoItemDoneRx    = regexp.MustCompile(`(?i)^\s*?-\s*?\[x\]\s+?.+$`)
+	todoItemNotDoneRx = regexp.MustCompile(`(?i)^\s*?-\s*?\[\s?\]\s+?.+$`)
+)
+
+type kind int
+
+// Sort sorts todo lists from r and outputs sorted to w
+func Sort(r io.Reader, w io.Writer) error {
+	emptyLinesCount := 0
+	prevIndent, curIndent := 0, 0
+
+	doneBuffer := []string{}
+	prevDone := false
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		lineKind := getLineKind(line)
+		curIndent = getIndentLen(line)
+
+		if prevDone && curIndent > prevIndent {
+			doneBuffer = append(doneBuffer, line)
+			continue
+		}
+		prevDone = false
+		prevIndent = curIndent
+
+		switch lineKind {
+		case done:
+			writeEmptyLines(w, emptyLinesCount)
+			emptyLinesCount = 0
+
+			doneBuffer = append(doneBuffer, line)
+
+			if !prevDone {
+				prevIndent = curIndent
+				prevDone = true
+			}
+		case notDone:
+			writeEmptyLines(w, emptyLinesCount)
+			emptyLinesCount = 0
+
+			fmt.Fprintln(w, line)
+		case blank:
+			emptyLinesCount++
+			if emptyLinesCount > 1 {
+				writeLines(w, doneBuffer)
+				writeEmptyLines(w, emptyLinesCount)
+
+				emptyLinesCount = 0
+				doneBuffer = []string{}
+			}
+		default:
+			writeLines(w, doneBuffer)
+			writeEmptyLines(w, emptyLinesCount)
+
+			emptyLinesCount = 0
+			doneBuffer = []string{}
+
+			fmt.Fprintln(w, line)
+		}
+	}
+	writeLines(w, doneBuffer)
+
+	return scanner.Err()
+}
+
+func getLineKind(line string) kind {
+	if todoItemDoneRx.MatchString(line) {
+		return done
+	}
+	if todoItemNotDoneRx.MatchString(line) {
+		return notDone
+	}
+	if whitespaceRx.MatchString(line) {
+		return blank
+	}
+	return other
+}
+
+func getIndentLen(s string) int {
+	n := 0
+	for _, c := range s {
+		if c == ' ' || c == '\t' {
+			n++
+			continue
+		}
+		break
+	}
+
+	return n
+}
 
 func writeLines(w io.Writer, lines []string) error {
 	for _, line := range lines {
@@ -30,54 +127,4 @@ func writeEmptyLines(w io.Writer, count int) error {
 		}
 	}
 	return nil
-}
-
-// Sort sorts todo lists from r and outputs sorted to w
-func Sort(r io.Reader, w io.Writer) error {
-	scanner := bufio.NewScanner(r)
-
-	emptyLinesCount := 0
-	doneBuffer := []string{}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		isDoneItem := todoItemDoneRx.MatchString(line)
-		isUndoneItem := todoItemUndoneRx.MatchString(line)
-		isBlankLine := whitespaceRx.MatchString(line) || line == ""
-
-		if isDoneItem {
-			writeEmptyLines(w, emptyLinesCount)
-			emptyLinesCount = 0
-			doneBuffer = append(doneBuffer, line)
-			continue
-		}
-		if isUndoneItem {
-			writeEmptyLines(w, emptyLinesCount)
-			emptyLinesCount = 0
-			fmt.Fprintln(w, line)
-			continue
-		}
-		if isBlankLine {
-			emptyLinesCount++
-			if emptyLinesCount > 1 {
-				writeLines(w, doneBuffer)
-				writeEmptyLines(w, emptyLinesCount)
-
-				emptyLinesCount = 0
-				doneBuffer = []string{}
-			}
-			continue
-		}
-
-		writeLines(w, doneBuffer)
-
-		emptyLinesCount = 0
-		doneBuffer = []string{}
-
-		fmt.Fprintln(w, line)
-	}
-	writeLines(w, doneBuffer)
-
-	return scanner.Err()
 }
--- pkg/todos/todo_list_test.go
+++ pkg/todos/todo_list_test.go
@@ -25,7 +25,7 @@ func TestTODORegexNotDone(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.expected, todoItemUndoneRx.MatchString(tt.given))
+			assert.Equal(t, tt.expected, todoItemNotDoneRx.MatchString(tt.given))
 		})
 	}
 }
@@ -61,7 +61,7 @@ func TestShouldSort(t *testing.T) {
 			`
 			- [] foo
 			- [] bazz
-			- [x] bar` + "\n",
+			- [x] bar`,
 			`
 			- [] foo
 			- [x] bar
@@ -71,7 +71,7 @@ func TestShouldSort(t *testing.T) {
 			- [] foo
 			- [] bazz
 			- [x] bar
-			more random text` + "\n",
+			more random text`,
 			`some random text
 			- [] foo
 			- [x] bar
@@ -85,7 +85,7 @@ func TestShouldSort(t *testing.T) {
 
 			- [] foobazz
 			- [x] foo
-			- [x] buzz` + "\n",
+			- [x] buzz`,
 			`
 			- [x] foo
 			- [] bar
@@ -101,7 +101,7 @@ func TestShouldSort(t *testing.T) {
 			random text
 			- [] buzz
 			- [] foobazz
-			- [x] foobar` + "\n",
+			- [x] foobar`,
 			`
 			- [x] foo
 			- [] bar
@@ -117,7 +117,7 @@ func TestShouldSort(t *testing.T) {
 
 			- [] buzz
 			- [] foobazz
-			- [x] foobar` + "\n",
+			- [x] foobar`,
 			`
 			- [x] foo
 			- [] bar
@@ -126,13 +126,67 @@ func TestShouldSort(t *testing.T) {
 			- [] buzz
 			- [x] foobar
 			- [] foobazz`},
+
+		{"hierarchical todos",
+			`foo bar
+				- [] bar
+				- [] buzz
+				- [] fuzzbuzz
+					- [] fuzzbuzz - 1
+					- [] fuzzbuzz - 2
+				- [] foobar
+				- [x] foo
+					- [x] sub foo
+					- [ ] sub bar
+				- [x] foobazz`,
+			`foo bar
+				- [] bar
+				- [x] foo
+					- [x] sub foo
+					- [ ] sub bar
+				- [] buzz
+				- [x] foobazz
+				- [] fuzzbuzz
+					- [] fuzzbuzz - 1
+					- [] fuzzbuzz - 2
+				- [] foobar`},
+		{"hierarchical todos with non-todo sub items",
+			`foo bar
+				- [] bar
+				- [] buzz
+				- [] foobar
+				- [x] foo
+					- foo context
+					more context
+				- [x] foobazz`,
+			`foo bar
+				- [] bar
+				- [x] foo
+					- foo context
+					more context
+				- [] buzz
+				- [x] foobazz
+				- [] foobar`},
+		{"empty line after list",
+			`
+				- [ ] foo
+				- [x] bar
+				- [x] buzzbar
+
+				text`,
+			`
+				- [x] bar
+				- [x] buzzbar
+				- [ ] foo
+
+				text`},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			var actualBuf bytes.Buffer
 			assert.NoError(t, Sort(bytes.NewBufferString(tt.given), &actualBuf))
-			assert.Equal(t, tt.expected, actualBuf.String())
+			assert.Equal(t, tt.expected+"\n", actualBuf.String())
 		})
 	}
 }
