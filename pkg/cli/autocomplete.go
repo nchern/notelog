afda2f3b9dde6048e7ae135bb71658ebbc074aee
nchern <nchern@inbox.ru>
2020-10-04 02:53:29 +0200

    adds a subcommand to list all subcommands to use it in shell autocomplete

--- pkg/cli/autocomplete.go
+++ pkg/cli/autocomplete.go
@@ -7,6 +7,6 @@ import (
 
 func autoCompleteScript() string {
 	name := os.Args[0]
-	return fmt.Sprintf("# Bash autocompletion for %s. Completes notes\ncomplete -W \"`%s -cmd=list`\" %s",
+	return fmt.Sprintf("# Bash autocompletion for %s. Completes notes\ncomplete -W \"`%s -c=list`\" %s",
 		name, name, name)
 }
