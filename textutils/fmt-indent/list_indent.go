da6d83be2a99a2e945dab0191cbd9e03bef8d70d
nchern <nchern@inbox.ru>
2020-10-29 01:34:54 +0100

    textutils: adds fmt-indent

---
+++ textutils/fmt-indent/list_indent.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+)
+
+var (
+	indent     = regexp.MustCompile(`^\s+?`)
+	listNumber = regexp.MustCompile(`(^\s*?\d+?\.\s*|^\s*?\d+?\s+?|^\s*?\*+\s*)`)
+)
+
+// Format formats identation list
+func Format(src io.Reader, dst io.Writer) error {
+	i := 0
+	itemIndent := ""
+	isListItem := false
+
+	p := &printer{w: dst}
+	scanner := bufio.NewScanner(src)
+
+	for scanner.Scan() {
+		if i > 0 {
+			p.Println()
+		}
+
+		l := scanner.Text()
+
+		if listNumber.MatchString(l) {
+			isListItem = true
+			itemIndent = strings.Repeat(" ", len(listNumber.FindString(l)))
+		} else {
+			l = strings.TrimSpace(l)
+			if isListItem {
+				if l != "" {
+					l = itemIndent + indent.ReplaceAllString(l, "")
+				}
+			}
+		}
+
+		p.Print(l)
+
+		if p.err != nil {
+			return p.err
+		}
+		i++
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type printer struct {
+	err error
+	w   io.Writer
+}
+
+func (p *printer) Printf(format string, a ...interface{}) {
+	if p.err != nil {
+		return
+	}
+	_, p.err = fmt.Fprintf(p.w, format, a...)
+}
+
+func (p *printer) Println() { p.Printf("\n") }
+
+func (p *printer) Print(s string) { p.Printf("%s", s) }
---
+++ textutils/fmt-indent/list_indent_test.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShouldFormat(t *testing.T) {
+
+	var tests = []struct {
+		name     string
+		expected string
+		given    string
+	}{
+		{"empty",
+			text(""),
+			text(""),
+		},
+		{"lines with spaces",
+			text(""),
+			text("   \t"),
+		},
+		{"subtext in numbered list",
+			text(
+				"abc",
+				"1. foo bar",
+				"   fuzz buzz",
+				"   subline",
+				"22. hello world",
+				"    - item 1",
+				"    - item 2",
+				"    subline2",
+			),
+			text(
+				"abc",
+				"1. foo bar",
+				"fuzz buzz",
+				"subline",
+				"22. hello world",
+				"- item 1",
+				" - item 2",
+				"  subline2",
+			),
+		},
+		{"subtext with tabs in numbered list",
+			text(
+				"abc",
+				"1. foo bar",
+				"   fuzz buzz",
+				"   subline",
+				"22. hello world",
+				"    - item 1",
+				"    - item 2",
+				"    subline2",
+			),
+			text(
+				"abc",
+				"1. foo bar",
+				"fuzz buzz",
+				"\t\t\tsubline",
+				"22. hello world",
+				"- item 1",
+				"\t- item 2",
+				"\t\t  subline2",
+			),
+		},
+		{"subtext under org mode headers",
+			text(
+				"abc",
+				"* Header 1",
+				"  lineA",
+				"  lineB",
+				"** Header 2",
+				"   - item 1",
+				"   - item 2",
+				"*** Header3",
+				"    lineA",
+				"    - item2",
+			),
+			text(
+				"abc",
+				"* Header 1",
+				" lineA",
+				"lineB",
+				"** Header 2",
+				"  - item 1",
+				" - item 2",
+				"*** Header3",
+				" \t lineA",
+				"\t- item2",
+			),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			actual := &bytes.Buffer{}
+
+			err := Format(bytes.NewBufferString(tt.given), actual)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, actual.String())
+		})
+	}
+}
+
+func text(lines ...string) string {
+	return strings.Join(lines, "\n")
+}
