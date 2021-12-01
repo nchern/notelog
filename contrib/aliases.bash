dd2f288fac20512b472834275b933d8a97967586
nchern <nchern@inbox.ru>
2021-12-01 21:34:01 +0100

    contrib: add experimental aliases for notelog search

--- contrib/aliases.bash
+++ contrib/aliases.bash
@@ -1,3 +1,7 @@
 # notelog search alias
 alias notesearch='notelog do search -i --color'
 alias notesearch-jump='notelog do search-browse'
+alias notesearch-go='notelog do search-browse'
+
+alias nls='notelog do search -i --color'
+alias nlsbrowse='notelog do search-browse'
