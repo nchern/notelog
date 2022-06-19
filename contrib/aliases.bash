16332b8c7df9c5a2b4a02eb37a68911e40686c75
nchern <nchern@inbox.ru>
2022-06-19 20:19:13 +0200

    contrib: simlification of search functionality wrappers
    aliases nls and browser -> nls script, that autodetects if it's a search
    request or browse request.

--- contrib/aliases.bash
+++ contrib/aliases.bash
@@ -3,7 +3,5 @@ alias notesearch='notelog do search -i --color'
 alias notesearch-jump='notelog do search-browse'
 alias notesearch-go='notelog do search-browse'
 
-alias nls='notelog do search -i --color'
-alias nlsbrowse='notelog do search-browse'
 
 alias nlgrep='notelog do grep -i --color'
