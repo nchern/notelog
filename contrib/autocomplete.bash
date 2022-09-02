581faa1bf31879ac34229c51dbeb9f134367eddc
nchern <nchern@inbox.ru>
2022-09-02 08:12:17 +0200

    contrib: add notelog-cat
    It is a shortcut script to call "notelog do cat"

--- contrib/autocomplete.bash
+++ contrib/autocomplete.bash
@@ -1,3 +1,4 @@
 # Bash autocompletion for notelog. Completes notes
 complete -C 'notelog do autocomplete' notelog
+complete -C 'notelog do autocomplete' notelog-cat
 complete -W "\`notes-select -ls\`" notes-select
