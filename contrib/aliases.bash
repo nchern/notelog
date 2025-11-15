a2a562dd34ea83f5c95def422c78a126c1c62843
nchern <nchern@inbox.ru>
2025-11-15 09:14:02 +0100

    chore: drop useless aliases

--- contrib/aliases.bash
+++ contrib/aliases.bash
@@ -1,7 +1,3 @@
 # notelog search alias
-alias notesearch='notelog search -i --color'
-alias notesearch-jump='notelog search-browse'
-alias notesearch-go='notelog search-browse'
-
 
 alias nlgrep='notelog grep -i --color'
